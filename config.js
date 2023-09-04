7ca797662457f27431e761b38b2e7796a9883c63
joshua.oh <joshua.oh@paywatchglobal.com>
2023-09-04 13:04:57 +0900

    add : firebaseconfig를 별개의 파일로 만들고 gitignore에 지정해놓음

---
+++ config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDeDG3C9i3BZLa8JXGTJcOUCla2rwSskik',
+  authDomain: 'test-c81cd.firebaseapp.com',
+  projectId: 'test-c81cd',
+  storageBucket: 'test-c81cd.appspot.com',
+  messagingSenderId: '485320566376',
+  appId: '1:485320566376:web:7b148995b1f40879c93ddd',
+};
