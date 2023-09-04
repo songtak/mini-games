69752834576b53d6ab8871a48c45e07b6eb970f0
joshua.oh <joshua.oh@paywatchglobal.com>
2023-09-04 17:34:38 +0900

    add : 페이워치 계정의 firebase, mixpanel 세팅 완료 - 주석처리 해놓음

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// Your web app's Firebase configuration
+// test 프로젝트용 config
 export const firebaseConfig = {
   apiKey: 'AIzaSyDeDG3C9i3BZLa8JXGTJcOUCla2rwSskik',
   authDomain: 'test-c81cd.firebaseapp.com',
@@ -7,3 +7,17 @@ export const firebaseConfig = {
   messagingSenderId: '485320566376',
   appId: '1:485320566376:web:7b148995b1f40879c93ddd',
 };
+
+// 페이워치 계정의 watch 프로젝트용 config
+// export const firebaseConfig = {
+//   apiKey: 'AIzaSyCdcvWDE4K8cMmTTCLPdYxigItucju8dFg',
+//   authDomain: 'albawatch-2b2da.firebaseapp.com',
+//   databaseURL: 'https://albawatch-2b2da.firebaseio.com',
+//   projectId: 'albawatch-2b2da',
+//   storageBucket: 'albawatch-2b2da.appspot.com',
+//   messagingSenderId: '869683140565',
+//   appId: '1:869683140565:web:318388b3685d6e2f684f5c',
+//   measurementId: 'G-VD419G6BZ2',
+// };
+
+export const mixpanelToken = 'e1f16bcbf0ef3f073bc3e9133d9ee7cd';
